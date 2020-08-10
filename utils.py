4066ab44b8f1464a1e63f50dec9e327587fe0c68
MERT DOĞAN <mertdogan@hotmail.com>
2020-08-10 16:50:55 +0300

    Update utils.py
    Açıklama satırlarını orijinal İngilizce bırak

--- utils.py
+++ utils.py
@@ -112,7 +112,7 @@ def show(name, orig, pause=1, resize=False, resetpos=None):
 
     h, w = img.shape[:2]
 
-    # Sonraki pencereye konumunu ayarla
+    # Set next window position
     margin = 25
     w += margin
     h += margin
@@ -210,8 +210,8 @@ def getPlotImg():
 def order_points(pts):
     rect = np.zeros((4, 2), dtype="float32")
 
-    # sol üst nokta en küçük toplamı alırken, 
-    # sağ alt nokta en büyük toplamı alır
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
     s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
@@ -219,44 +219,45 @@ def order_points(pts):
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
 
-    # sıralı koordinatları döndür
+    # return the ordered coordinates
     return rect
 
 
 def four_point_transform(image, pts):
-    # işaretlemelerin tutarlı bir sırasını elde et
-    # ve bunları ayrı ayrı aç
+    # obtain a consistent order of the points and unpack them
+    # individually
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
 
-    # yeni resmin genişliğini hesapla
+    # compute the width of the new image, which will be the
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
 
     maxWidth = max(int(widthA), int(widthB))
     # maxWidth = max(int(np.linalg.norm(br-bl)), int(np.linalg.norm(tr-tl)))
 
-    # yeni resmin yüksekliğini hesapla
+    # compute the height of the new image, which will be the
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
     # maxHeight = max(int(np.linalg.norm(tr-br)), int(np.linalg.norm(tl-br)))
 
-    # artık yeni görüntünün boyutlarına sahibiz, construct
-    # "kuş bakışı görünümü" elde etmek için resmin hedef noktalarını oluştur
-    # (örneğin sol-alt görünümü), yine sol üst, sağ üst, sağ alt ve sol alt 
-    # sıradaki noktaları belirle
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
     dst = np.array([
         [0, 0],
         [maxWidth - 1, 0],
         [maxWidth - 1, maxHeight - 1],
         [0, maxHeight - 1]], dtype="float32")
 
-    # perspektif dönüşüm matrisini hesapla ve ardından uygula
+    # compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
-    # çarpık görüntüyü döndür
+    # return the warped image
     return warped
 
 
@@ -300,7 +301,7 @@ def angle(p1, p2, p0):
 
 
 def checkMaxCosine(approx):
-    # 4 işaretçinin olduğunu varsayar
+    # assumes 4 pts present
     maxCosine = 0
     minCosine = 1.5
     for i in range(2, 5):
@@ -321,10 +322,10 @@ def validateRect(approx):
 
 
 def auto_canny(image, sigma=0.93):
-    # tek kanal piksel yoğunluklarının medyanını hesapla
+    # compute the median of the single channel pixel intensities
     v = np.median(image)
 
-    # hesaplanan medyanı kullanarak otomatik Canny kenarı algılama uygula
+    # apply automatic Canny edge detection using the computed median
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
     edged = cv2.Canny(image, lower, upper)
